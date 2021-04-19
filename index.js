d33c4c95420bafc5feeab65e4399f19cc4111f5c
Jamey Kirk <jameykirk@gmail.com>
2021-04-19 11:09:54 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function parseScores(scoresString) {
+   return toString(scoresString).split(" ");
+}
+
+function buildDistributionArray(scoresArray) {
+   // TODO: Compete the function
+}
+
+function setTableContent(userInput) {
+   // TODO: Compete the function
+}
+
+// The argument can be changed for testing purposes
+setTableContent("45 78 98 83 86 99 90 59");
