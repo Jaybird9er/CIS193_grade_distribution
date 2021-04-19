795faf8d1aa77c0d0b1315989f3d14f75fb9cc2d
Jamey Kirk <jameykirk@gmail.com>
2021-04-19 11:21:41 +0900

    parseScore; buildDistributionArray functions done

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ function parseScores(scoresString) {
 }
 
 function buildDistributionArray(scoresArray) {
-   // TODO: Compete the function
+   var scoresDistrubution = [0, 0, 0, 0, 0];
+   if (scoresArray.length === 0) {
+      return scoresDistrubution;
+   }
+   else {
+      for (var i = 0; i < scoresArray.length; i++) {
+         if (scoresArray[i] <= 100 && scoresArray[i] >= 90)
+            scoresDistrubution[0]++;
+         else if (scoresArray[i] <= 89 && scoresArray[i] >= 80)
+            scoresDistrubution[1]++;
+         else if (scoresArray[i] <= 79 && scoresArray[i] >= 70)
+            scoresDistrubution[2]++;
+         else if (scoresArray[i] <= 69 && scoresArray[i] >= 60)
+            scoresDistrubution[3]++;
+         else if (scoresArray[i] <= 59 && scoresArray[i] >= 0)
+            scoresDistrubution[4]++;
+      }
+   }
+   return scoresDistrubution;
 }
 
 function setTableContent(userInput) {
