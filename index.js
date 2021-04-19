694bbb8233adfea3f0f075719cb0de36ba3a2030
Jamey Kirk <jameykirk@gmail.com>
2021-04-19 12:52:41 +0900

    job done

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 function parseScores(scoresString) {
-   return toString(scoresString).split(" ");
+   return scoresString.split(" ");
 }
 
 function buildDistributionArray(scoresArray) {
    var scoresDistrubution = [0, 0, 0, 0, 0];
-   if (scoresArray.length === 0) {
+   if (scoresArray.length === 0 
+      && parseInt(scoresArray[0]) === NaN
+      && parseInt(scoresArray[1]) === NaN
+      && parseInt(scoresArray[2]) === NaN
+      && parseInt(scoresArray[3]) === NaN
+      && parseInt(scoresArray[4]) === NaN) {
       return scoresDistrubution;
    }
    else {
       for (var i = 0; i < scoresArray.length; i++) {
-         if (scoresArray[i] <= 100 && scoresArray[i] >= 90)
+         var value = parseInt(scoresArray[i]);
+         if (value <= 100 && value >= 90)
             scoresDistrubution[0]++;
-         else if (scoresArray[i] <= 89 && scoresArray[i] >= 80)
+         else if (value <= 89 && value >= 80)
             scoresDistrubution[1]++;
-         else if (scoresArray[i] <= 79 && scoresArray[i] >= 70)
+         else if (value <= 79 && value >= 70)
             scoresDistrubution[2]++;
-         else if (scoresArray[i] <= 69 && scoresArray[i] >= 60)
+         else if (value <= 69 && value >= 60)
             scoresDistrubution[3]++;
-         else if (scoresArray[i] <= 59 && scoresArray[i] >= 0)
+         else if (value <= 59 && value >= 0)
             scoresDistrubution[4]++;
       }
    }
@@ -25,7 +31,24 @@ function buildDistributionArray(scoresArray) {
 }
 
 function setTableContent(userInput) {
-   // TODO: Compete the function
+   var scores = parseScores(userInput);
+   var table = '';
+   if (scores == "") {
+      document.getElementById("distributionTable").innerHTML = '<td>No graph to display</td>';
+   }
+   else if (scores.length > 0) {
+      var scoresDistribution = buildDistributionArray(scores);
+      table = '<tr>';
+      for (var i = 0; i < scoresDistribution.length; i++) {
+         table += '<td><div style="height:' + scoresDistribution[i] * 10 + 'px" class="bar' + i + '"></div></td>';
+      }
+      table += '</tr>';
+      table += '<tr><td>A</td><td>B</td><td>C</td><td>D</td><td>F</td></tr>';
+      for (var i = 0; i < scoresDistribution.length; i++) {
+         table += '<td>' + scoresDistribution[i] + '</td>';
+      }
+      document.getElementById("distributionTable").innerHTML += table + '</tr>';
+   }
 }
 
 // The argument can be changed for testing purposes
